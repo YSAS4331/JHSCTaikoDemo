2a215d03aa9b1a117b2476573449f800a8ef896a
YSAS4331 <ryo.matsu0619@gmail.com>
2025-12-20 14:53:39 +0000

    Update index.html, Home.js

--- Home.js
+++ Home.js
@@ -6,6 +6,19 @@
     const url = new URL(urlStr);
     return url.searchParams.get(key);
   };
+  const createList = v => {
+    return `
+      <div class="listItem">
+        <a href="${v.url}" title="${v.title}" target="_blank" rel="noopener noreferrer"><img src="https://img.youtube.com/vi/${getQueryParam(v.url, 'v')}/maxresdefault.jpg" alt="サムネイル画像" style="width: 100%; height: auto; aspect-ratio: 16/9"></a>
+        <label class="view">${v.viewCount}</label>
+        <label class="like">${v.likeCount}</label>
+      </div>
+    `;
+  };
+  const appendList = (id, html) => {
+    $(`#${id}`).innerHTML = html;
+    $$(`#${id} > .listItem`).forEach(el => window.observer.observe(el));
+  };
 
   const popular = [...datas].sort((a, b) => {
     const scoreA = a.likeCount * 100 + a.viewCount;
@@ -13,16 +26,8 @@
     return scoreB - scoreA;
   }).slice(0, 10);
 
-  const likeCountHtml = popular.map(video => {
-    return `
-      <div class="listItem">
-        <a href="${video.url}" title="${video.title}" target="_blank" rel="noopener noreferrer"><img src="https://img.youtube.com/vi/${getQueryParam(video.url, 'v')}/maxresdefault.jpg" alt="サムネイル画像" style="width: 100%; height: auto; aspect-ratio: 16/9"></a>
-        <label class="view">${video.viewCount}</label>
-        <label class="like">${video.likeCount}</label>
-      </div>
-    `;
-  }).join('');
+  appendList('likeCount', popular.map(video => createList(video)).join(''));
 
-  $('#likeCount').innerHTML = likeCountHtml;
-  $$('#likeCount > .listItem').forEach(el => window.observer.observe(el));
+  const newVideo = datas.slice(0, 10).map(video => createList(video)).join('');
+  appendList('newVideo', newVideo);
 })();
