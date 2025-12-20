93abc57eb09b54c2198e3160c938807126025caa
YSAS4331 <ryo.matsu0619@gmail.com>
2025-12-20 02:19:45 +0000

    Update Home.js, index.html

--- Home.js
+++ Home.js
@@ -1,3 +1,23 @@
 (async () => {
-  const res = await fetch(./videos.json);
+  const { $ } = window.modules;
+  const res = await fetch('./videos.json');
+  const datas = await res.json();
+
+  const popular = [...datas].sort((a, b) => {
+    const scoreA = a.likeCount * 100 + a.viewCount;
+    const scoreB = b.likeCount * 100 + b.viewCount;
+    return scoreB - scoreA;
+  }).slice(0, 10);
+
+  const likeCountHtml = popular.map(video => {
+    return `
+      <div class="listItem">
+        <a href="${video.url}">${video.title}</a>
+        <div class="like">${video.likeCount}</div>
+        <div class="view">${video.viewCount}</div>
+      </div>
+    `;
+  }).join('');
+
+  $('#likeCount').innerHTML = likeCountHtml;
 })();
